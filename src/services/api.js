54b6da8f9765c097991cad811e331c0f9a079f20
Luan Rodrigues Tavares <luanrodrigues.contatoprofissional@outlook.com>
2025-07-24 20:42:35 -0300

    rota backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001',
+    baseURL: 'http://162.240.235.56:3001',
 });
 
 api.interceptors.request.use((config) => {
